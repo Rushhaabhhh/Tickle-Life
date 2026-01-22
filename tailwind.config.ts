7b37999dba8d6baede875e1ddf5f2d8f404360a8
Rushhaabhhh <rushabhmistry16@gmail.com>
2026-01-23 03:06:46 +0530

    Fixed Inter font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,6 @@ const config: Config = {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        inter: ['var(--font-inter)', 'sans-serif']
-      }
     }
   }
 };
